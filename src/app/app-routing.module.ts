dc7585dc7945a934e97b5c167c690384fdea7ed3
Ivan Oliven <si.ivandeoliveira21@gmail.com>
2022-12-11 20:48:47 -0300

    modulo renderizando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '',pathMatch:"full", redirectTo:'processos'},
+  {
+    path: 'processos',
+    loadChildren: () => import('./processos/processos.module').then(m => m.ProcessosModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
