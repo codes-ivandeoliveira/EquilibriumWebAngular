e752d84b85798b8757ac828511a274471eb68fee
Ivan Oliven <si.ivandeoliveira21@gmail.com>
2022-12-11 21:24:17 -0300

    layout tabela

--- src/app/processos/processos.module.ts
+++ src/app/processos/processos.module.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 
 import { ProcessosRoutingModule } from './processos-routing.module';
 import { ProcessosComponent } from './processos/processos.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 
 
@@ -14,7 +18,9 @@ import { ProcessosComponent } from './processos/processos.component';
   imports: [
     CommonModule,
     ProcessosRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class ProcessosModule { }
